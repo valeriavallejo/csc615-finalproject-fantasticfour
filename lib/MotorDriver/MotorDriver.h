82d088cd13de065ca0e4ca643820c1993edf1efd
celiarr <78006504+celiarr@users.noreply.github.com>
2022-05-09 11:42:40 -0700

    Add files via upload

--- lib/MotorDriver/MotorDriver.h
+++ lib/MotorDriver/MotorDriver.h
@@ -1,23 +1,29 @@
 /**************************************************************
  * Class: CSC-615-01 Spring 2022
- * Name:Celia Ramirez
- * Student ID:918689985
- * Github ID:celiarr
- * Project: Assignment 3 - Motors & Motor Shield*
+ * Name:
+ * Student ID:
+ * Github ID:
+ * Project:
  * File: MotorDriver.h
- *  Description:
+ * Description:
  * **************************************************************/
 #ifndef __TB6612FNG_
 #define __TB6612FNG_
 
 #include "DEV_Config.h"
 #include "PCA9685.h"
-
-#define PWM        PCA_CHANNEL_0
+//MOTORA
+#define PWMA        PCA_CHANNEL_0
 #define AIN1        PCA_CHANNEL_1
 #define AIN2        PCA_CHANNEL_2
+//MOTORB
+#define PWMB        PCA_CHANNEL_5
+#define BIN1        PCA_CHANNEL_3
+#define BIN2        PCA_CHANNEL_4
 
-#define MOTOR       0
+#define MOTORA       0  //MA1 &MA2
+#define MOTORB       1    //MB1 & MB2
+
 
 void MotorInit(void);
 void MotorForward(UBYTE motor, UWORD speed);
