e04e6cc36b73d6d408f4d83cf32de17c6696fb68
celiarr <78006504+celiarr@users.noreply.github.com>
2022-05-20 21:03:52 -0700

    Update distance.h

--- distance.h
+++ distance.h
@@ -1,11 +1,13 @@
 /**************************************************************
  * Class: CSC-615-01 Spring 2022
- * Name:
+ * Name:fantastic four
  * Student ID:
  * Github ID:
- * Project:
+ * Project:robot car
  * File: distance.h
- * Description:
+ * Description:this file contains a program that runs three threads that control
+ three ultrasonic sensors. Each thread exits when they detect an object
+ that is less than or equal to 10cm.
  * **************************************************************/
 #ifndef distance_h
 #define distance_h
@@ -13,17 +15,15 @@
 #include <unistd.h>
 #include <wiringPi.h>
 #include <stdbool.h>
-
-#define ECHOONE 5 //GPIO24/WPI 5
+//left ultrasonic sensor
+#define ECHOONE 5 //GPIO24/WPI 5 
 #define TRIGGERONE 6 //GPIO25/WPI 6
+//middle ultrasonic sensor
 #define ECHOTWO 2 //GPIO27/WPI 2
 #define TRIGGERTWO 0 //GPIO17/WPI 0
+//right ultrasonic sensor
 #define ECHOTHREE 28 //GPIO2O/WPI 28
 #define TRIGGERTHREE 26 //GPIO12/WPI 26
-
-int measurmentOne; //left
-int measurmentTwo; //middle
-int measurmentThree; //right
 
 int threadOne;
 int threadTwo;
@@ -37,19 +37,19 @@ void *distanceThree;
 pthread_t one;
 pthread_t two;
 pthread_t three;
-
+//used to help exit the methods being run my the threads
 bool flagOne;
 bool flagTwo;
 bool flagThree;
-
+///a method for each thread to run
 void *distanceSensorOne(void *arg);
 void *distanceSensorTwo(void *arg);
 void *distanceSensorThree(void *arg);
-
+//each function initializes a thread
 void threadOneInIt();
 void threadTwoInIt();
 void threadThreeInIt();
-
+//initializes all three threads
 void inItDistanceThreads();
 
 #endif /* distance_h */
