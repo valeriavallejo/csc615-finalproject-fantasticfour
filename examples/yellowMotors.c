3ed7570f9b501c045d4079a9f7066177710a5ba0
celiarr <78006504+celiarr@users.noreply.github.com>
2022-05-20 21:30:17 -0700

    Update yellowMotors.c

--- examples/yellowMotors.c
+++ examples/yellowMotors.c
@@ -1,39 +1,61 @@
 /**************************************************************
  * Class: CSC-615-01 Spring 2022
- * Name:
+ * Name:fantastic four
  * Student ID:
  * Github ID:
- * Project:
+ * Project:robot car
  * File: yellowMotors.c
- * Description:
+ * Description:this file contains functions that control which direction a DC motor rotates. 
+ * It contains functions that a allow a motor to rotate either forward or backward
  * **************************************************************/
 #include "yellowMotors.h"
+#include "yellowMotors.h"
 
-
+//initializes the motor hat used to manage the DC motors
 void inItYellowMotors() {
 
   wiringPiSetup();
-  
+
 if (DEV_ModuleInit())
-        exit(0);
+exit(0);
     MotorInit();
+    MotorInitOne();
 }
+//this functions makes a motor go forward at the speed entered 
 void  motorsForward(int speed){
     MotorForward(MOTORA,speed);
-    
+
     MotorForward(MOTORB,speed);
+  MotorForward(MOTORC,speed);
+
+    MotorForward(MOTORD,speed);
 }
+//this function makes a motor go backwards at the speed entered
  void motorsBackward(int speed){
     MotorBackward(MOTORA,speed);
-    
+
     MotorBackward(MOTORB,speed);
+    MotorBackward(MOTORC,speed);
+
+    MotorBackward(MOTORD,speed);
 }
-  
-    
+
+//uninitializes all motors 
  void exitYellowMotors(){
     MotorStop(MOTORA);
     MotorStop(MOTORB);
-    
+MotorStop(MOTORC);
+    MotorStop(MOTORD);
   DEV_ModuleExit();
+  }
+//test case for the motors
+int main(void){
+speed = 40;
+inItYellowMotors();
+motorsForward(speed);
+delay(10000);
+ motorsBackward(speed);
+delay(10000);
+exitYellowMotors();
+return 0;
 }
-//delay(1000); use in main file
